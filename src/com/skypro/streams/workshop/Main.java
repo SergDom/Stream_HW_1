67bb2b0acbcf8a704c201be7d4a565f564628765
sergeydomanov <nomad-labs@yandex.ru>
2022-11-08 20:18:30 +0300

    new committ

--- src/com/skypro/streams/workshop/Main.java
+++ src/com/skypro/streams/workshop/Main.java
@@ -3,12 +3,7 @@ package com.skypro.streams.workshop;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
